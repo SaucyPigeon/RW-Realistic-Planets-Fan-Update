e1b564cf746ef5a0244a1cbf9323dddd7b88eb95
SaucyPigeon <emilianb@hotmail.co.uk>
2020-02-28 16:54:41 +0000

    Added 1.1 project, updated to 1.1, remove old assemblies

---
+++ Source/Rainbeau's Realistic Planets/Rainbeau's Realistic Planets-RW1.1/Controller.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using Verse;
+
+namespace Planets_Code
+{
+	public class Controller : Mod {
+		public static Dictionary<Faction, int> factionCenters = new Dictionary<Faction, int>();
+		public static Dictionary<Faction, int> failureCount = new Dictionary<Faction, int>();
+		public static double maxFactionSprawl = 0;
+		public static double minFactionSeparation = 0;
+		public static Settings Settings;
+		public override string SettingsCategory() { return "Planets.ModName".Translate(); }
+		public override void DoSettingsWindowContents(Rect canvas) { Settings.DoWindowContents(canvas); }
+
+		public Controller(ModContentPack content) : base(content) {
+			const bool Debug = true;
+			const string Id = "net.rainbeau.rimworld.mod.realisticplanets";
+
+			if (Debug)
+			{
+				Log.Warning($"{Id} is in debug mode. Contact the mod author if you see this.");
+				Harmony.DEBUG = Debug;
+			}
+
+			var harmony = new Harmony(Id);
+			harmony.PatchAll( Assembly.GetExecutingAssembly() );
+			Settings = GetSettings<Settings>();
+		}
+	}
+	
+}
