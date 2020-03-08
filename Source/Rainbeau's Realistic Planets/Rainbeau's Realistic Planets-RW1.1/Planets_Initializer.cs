daac3560fe7d693dbb786ee25c20ee78d1cc0067
SaucyPigeon <emilianb@hotmail.co.uk>
2020-03-08 18:27:05 +0000

    Added winsize increase, refactor

--- Source/Rainbeau's Realistic Planets/Rainbeau's Realistic Planets-RW1.1/Planets_Initializer.cs
+++ Source/Rainbeau's Realistic Planets/Rainbeau's Realistic Planets-RW1.1/Planets_Initializer.cs
@@ -1,67 +1,69 @@
 ﻿using RimWorld;
 using System.Linq;
 using Verse;
+using HarmonyLib;
+using RimWorld.Planet;
+using UnityEngine;
 
 namespace Planets_Code
 {
 	[StaticConstructorOnStartup]
-	internal static class Planets_Initializer {
-		static Planets_Initializer() {
-			if (ModsConfig.ActiveModsInLoadOrder.Any(mod => mod.Name.Contains("My Little Planet"))) {
-				Controller.Settings.usingMLP = true;
-			}
-			if (ModsConfig.ActiveModsInLoadOrder.Any(mod => mod.Name.Contains("Faction Control"))) {
-				Controller.Settings.usingFactionControl = true;
-			}
-			if (ModsConfig.ActiveModsInLoadOrder.Any(mod => mod.Name.Contains("Nature's Pretty Sweet"))) {
-				Controller.Settings.otherGrassland = true;
-				Controller.Settings.otherSavanna = true;
-			}
-			if (ModsConfig.ActiveModsInLoadOrder.Any(mod => mod.Name.Contains("Terra Project (Core)"))) {
-				Controller.Settings.otherSavanna = true;
-			}
-			if (ModsConfig.ActiveModsInLoadOrder.Any(mod => mod.Name.Contains("Advanced Biomes"))) {
-				Controller.Settings.otherSavanna = true;
-			}
-			foreach (ThingDef current in DefDatabase<ThingDef>.AllDefsListForReading) {
-				if (current.plant != null) {
-					if (current.plant.wildBiomes != null) {
-						for (int j = 0; j < current.plant.wildBiomes.Count; j++) {
-							if (current.plant.wildBiomes[j].biome.defName == "Tundra") {
+	internal static class Planets_Initializer
+	{
+		private static void EditPlantDefs()
+		{
+			foreach (ThingDef current in DefDatabase<ThingDef>.AllDefsListForReading)
+			{
+				if (current.plant != null)
+				{
+					if (current.plant.wildBiomes != null)
+					{
+						for (int j = 0; j < current.plant.wildBiomes.Count; j++)
+						{
+							if (current.plant.wildBiomes[j].biome.defName == "Tundra")
+							{
 								PlantBiomeRecord newRecord = new PlantBiomeRecord();
 								newRecord.biome = BiomeDef.Named("RRP_Permafrost");
-								newRecord.commonality = current.plant.wildBiomes[j].commonality/2;
+								newRecord.commonality = current.plant.wildBiomes[j].commonality / 2;
 								current.plant.wildBiomes.Add(newRecord);
 							}
-							if (current.plant.wildBiomes[j].biome.defName == "TemperateForest") {
-								if (!current.defName.Contains("Tree")) {
+							if (current.plant.wildBiomes[j].biome.defName == "TemperateForest")
+							{
+								if (!current.defName.Contains("Tree"))
+								{
 									PlantBiomeRecord newRecord = new PlantBiomeRecord();
 									newRecord.biome = BiomeDef.Named("RRP_Grassland");
-									newRecord.commonality = current.plant.wildBiomes[j].commonality/2;
+									newRecord.commonality = current.plant.wildBiomes[j].commonality / 2;
 									current.plant.wildBiomes.Add(newRecord);
 								}
 							}
-							if (current.plant.wildBiomes[j].biome.defName == "BorealForest") {
-								if (!current.defName.Contains("Tree")) {
+							if (current.plant.wildBiomes[j].biome.defName == "BorealForest")
+							{
+								if (!current.defName.Contains("Tree"))
+								{
 									PlantBiomeRecord newRecord = new PlantBiomeRecord();
 									newRecord.biome = BiomeDef.Named("RRP_Steppes");
-									newRecord.commonality = current.plant.wildBiomes[j].commonality/2;
+									newRecord.commonality = current.plant.wildBiomes[j].commonality / 2;
 									current.plant.wildBiomes.Add(newRecord);
 								}
 							}
-							if (current.plant.wildBiomes[j].biome.defName == "AridShrubland") {
-								if (!current.defName.Contains("Acacia")) {
+							if (current.plant.wildBiomes[j].biome.defName == "AridShrubland")
+							{
+								if (!current.defName.Contains("Acacia"))
+								{
 									PlantBiomeRecord newRecord = new PlantBiomeRecord();
 									newRecord.biome = BiomeDef.Named("RRP_Savanna");
-									newRecord.commonality = current.plant.wildBiomes[j].commonality/2;
+									newRecord.commonality = current.plant.wildBiomes[j].commonality / 2;
 									current.plant.wildBiomes.Add(newRecord);
 								}
 							}
-							if (current.plant.wildBiomes[j].biome.defName == "Desert") {
-								if (!current.defName.Contains("Acacia")) {
+							if (current.plant.wildBiomes[j].biome.defName == "Desert")
+							{
+								if (!current.defName.Contains("Acacia"))
+								{
 									PlantBiomeRecord newRecord = new PlantBiomeRecord();
 									newRecord.biome = BiomeDef.Named("RRP_TemperateDesert");
-									newRecord.commonality = current.plant.wildBiomes[j].commonality/2;
+									newRecord.commonality = current.plant.wildBiomes[j].commonality / 2;
 									current.plant.wildBiomes.Add(newRecord);
 									PlantBiomeRecord newRecord2 = new PlantBiomeRecord();
 									newRecord2.biome = BiomeDef.Named("RRP_Oasis");
@@ -73,37 +75,49 @@ namespace Planets_Code
 					}
 				}
 			}
-			foreach (PawnKindDef current in DefDatabase<PawnKindDef>.AllDefs) {
-				if (current.RaceProps.wildBiomes != null && current.defName != "Cobra") {
-					for (int j = 0; j < current.RaceProps.wildBiomes.Count; j++) {
-						if (current.RaceProps.wildBiomes[j].biome.defName == "Tundra") {
+		}
+
+		private static void EditAnimalDefs()
+		{
+			foreach (PawnKindDef current in DefDatabase<PawnKindDef>.AllDefs)
+			{
+				if (current.RaceProps.wildBiomes != null && current.defName != "Cobra")
+				{
+					for (int j = 0; j < current.RaceProps.wildBiomes.Count; j++)
+					{
+						if (current.RaceProps.wildBiomes[j].biome.defName == "Tundra")
+						{
 							AnimalBiomeRecord newRecord = new AnimalBiomeRecord();
 							newRecord.biome = BiomeDef.Named("RRP_Permafrost");
-							newRecord.commonality = current.RaceProps.wildBiomes[j].commonality/2;
+							newRecord.commonality = current.RaceProps.wildBiomes[j].commonality / 2;
 							current.RaceProps.wildBiomes.Add(newRecord);
 						}
-						if (current.RaceProps.wildBiomes[j].biome.defName == "TemperateForest") {
+						if (current.RaceProps.wildBiomes[j].biome.defName == "TemperateForest")
+						{
 							AnimalBiomeRecord newRecord = new AnimalBiomeRecord();
 							newRecord.biome = BiomeDef.Named("RRP_Grassland");
-							newRecord.commonality = current.RaceProps.wildBiomes[j].commonality/2;
+							newRecord.commonality = current.RaceProps.wildBiomes[j].commonality / 2;
 							current.RaceProps.wildBiomes.Add(newRecord);
 						}
-						if (current.RaceProps.wildBiomes[j].biome.defName == "BorealForest") {
+						if (current.RaceProps.wildBiomes[j].biome.defName == "BorealForest")
+						{
 							AnimalBiomeRecord newRecord = new AnimalBiomeRecord();
 							newRecord.biome = BiomeDef.Named("RRP_Steppes");
-							newRecord.commonality = current.RaceProps.wildBiomes[j].commonality/2;
+							newRecord.commonality = current.RaceProps.wildBiomes[j].commonality / 2;
 							current.RaceProps.wildBiomes.Add(newRecord);
 						}
-						if (current.RaceProps.wildBiomes[j].biome.defName == "AridShrubland") {
+						if (current.RaceProps.wildBiomes[j].biome.defName == "AridShrubland")
+						{
 							AnimalBiomeRecord newRecord = new AnimalBiomeRecord();
 							newRecord.biome = BiomeDef.Named("RRP_Savanna");
-							newRecord.commonality = current.RaceProps.wildBiomes[j].commonality/2;
+							newRecord.commonality = current.RaceProps.wildBiomes[j].commonality / 2;
 							current.RaceProps.wildBiomes.Add(newRecord);
 						}
-						if (current.RaceProps.wildBiomes[j].biome.defName == "Desert") {
+						if (current.RaceProps.wildBiomes[j].biome.defName == "Desert")
+						{
 							AnimalBiomeRecord newRecord = new AnimalBiomeRecord();
 							newRecord.biome = BiomeDef.Named("RRP_TemperateDesert");
-							newRecord.commonality = current.RaceProps.wildBiomes[j].commonality/2;
+							newRecord.commonality = current.RaceProps.wildBiomes[j].commonality / 2;
 							current.RaceProps.wildBiomes.Add(newRecord);
 							AnimalBiomeRecord newRecord2 = new AnimalBiomeRecord();
 							newRecord2.biome = BiomeDef.Named("RRP_Oasis");
@@ -114,6 +128,42 @@ namespace Planets_Code
 				}
 			}
 		}
+
+		private static void IncreasePlanetTabWinSize()
+		{
+			var winSizeField = AccessTools.Field(typeof(WITab_Planet), "WinSize");
+			var winSize = (Vector2)winSizeField.GetValue(null);
+			winSize.y *= 2;
+			winSizeField.SetValue(null, winSize);
+		}
+
+		static Planets_Initializer()
+		{
+			if (ModsConfig.ActiveModsInLoadOrder.Any(mod => mod.Name.Contains("My Little Planet")))
+			{
+				Controller.Settings.usingMLP = true;
+			}
+			if (ModsConfig.ActiveModsInLoadOrder.Any(mod => mod.Name.Contains("Faction Control")))
+			{
+				Controller.Settings.usingFactionControl = true;
+			}
+			if (ModsConfig.ActiveModsInLoadOrder.Any(mod => mod.Name.Contains("Nature's Pretty Sweet")))
+			{
+				Controller.Settings.otherGrassland = true;
+				Controller.Settings.otherSavanna = true;
+			}
+			if (ModsConfig.ActiveModsInLoadOrder.Any(mod => mod.Name.Contains("Terra Project (Core)")))
+			{
+				Controller.Settings.otherSavanna = true;
+			}
+			if (ModsConfig.ActiveModsInLoadOrder.Any(mod => mod.Name.Contains("Advanced Biomes")))
+			{
+				Controller.Settings.otherSavanna = true;
+			}
+			EditPlantDefs();
+			EditAnimalDefs();
+			IncreasePlanetTabWinSize();
+
+		}
 	}
-	
 }
