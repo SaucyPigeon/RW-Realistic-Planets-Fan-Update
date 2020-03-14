e6863b8fd52e35c70760a44bfbdcb82d58aac6c3
SaucyPigeon <emilianb@hotmail.co.uk>
2020-03-14 11:57:52 +0000

    Harmony is now an external dependency
    Also updated RimWorld reference assemblies.

--- Source/Rainbeau's Realistic Planets/Rainbeau's Realistic Planets-RW1.1/Controller.cs
+++ Source/Rainbeau's Realistic Planets/Rainbeau's Realistic Planets-RW1.1/Controller.cs
@@ -24,8 +24,6 @@ namespace Planets_Code
 			const bool Debug = false;
 			const string Id = "net.rainbeau.rimworld.mod.realisticplanets";
 
-			Log.Warning($"Realistic Planets - Fan Update will be using Harmony as an external dependency when it next updates. To avoid wiping your mod list, please download and use Harmony now.");
-
 			if (Debug)
 			{
 				Log.Warning($"{Id} is in debug mode. Contact the mod author if you see this.");
