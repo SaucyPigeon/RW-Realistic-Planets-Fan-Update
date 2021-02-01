9a4b395d01c571b5286b25e578ed89b1c03bb540
SaucyPigeon <emilianb@hotmail.co.uk>
2021-02-01 22:08:53 +0000

    Adjusted stormy grasslands, removed texture change. 1.2 only currently.

--- Source/Rainbeau's Realistic Planets/Rainbeau's Realistic Planets-RW1.1/BiomeWorkers/BiomeWorker_MVB_StormyGrasslands.cs
+++ Source/Rainbeau's Realistic Planets/Rainbeau's Realistic Planets-RW1.1/BiomeWorkers/BiomeWorker_MVB_StormyGrasslands.cs
@@ -1,8 +1,9 @@
 ﻿using RimWorld;
 using RimWorld.Planet;
 using Verse;
+using System;
 
-namespace Planets_Code.BiomeWorkers
+namespace Planets_Code
 {
 	/// <summary>
 	/// The special BiomeWorker for More Vanilla Biome's grasslands. Note that
@@ -24,9 +25,28 @@ namespace Planets_Code.BiomeWorkers
 					return -100f;
 				}
 			}
-
-			throw new System.Exception("This BiomeWorker is not fully implemented.");
-			return 100f;
+			if (tile.WaterCovered)
+			{
+				return -100f;
+			}
+			if (tile.temperature <= 18f)
+			{
+				return 0f;
+			}
+			if (tile.temperature > 0f && (tile.rainfall / tile.temperature < 28f))
+			{
+				return 0f;
+			}
+			if (tile.rainfall < 1200f)
+			{
+				return 0f;
+			}
+			float tempAdjust = TempCheck.SeasonalTempChange(tileID);
+			if (tile.rainfall >= 2000f && (tile.temperature - tempAdjust <= 42f))
+			{
+				return 0f;
+			}
+			return 22.5f + (tile.temperature - 20f) * 2.2f + (tile.rainfall - 1200f) / 50f;
 		}
 	}
 }
